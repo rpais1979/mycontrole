e5b63941e2f130f05468d929ecb5e60ea5ae99eb
rpais1979 <rodrigo.pais.28@gmail.com>
2018-08-26 20:32:02 -0300

    Atualização do GIT
    Atualização do GIT

---
+++ vendor/class/php-classes/src/PageAdmin.php
@@ -0,0 +1,12 @@
+<?php 
+ 
+namespace Mycontrole;
+
+class PageAdmin extends Page{
+
+	public function __construct($opts = array(), $tpl_dir = "/views/admin/"){
+
+		parent::__construct($opts, $tpl_dir);
+	}
+}
+?>
